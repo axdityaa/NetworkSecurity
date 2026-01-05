6795ecfcd862513b00b4acd2e250ab74fef701a2
Aditya <adityadube9284@gmail.com>
2026-01-05 18:23:39 +0530

    Data transformation Done

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingetion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -21,5 +22,11 @@ if __name__ == "__main__":
         data_validation_artifact = data_validation.intitiate_data_validation()
         logging.info("Data validation completed")
         print(data_validation_artifact)
+        logging.info("Starting data transformation")
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_transformation_config,data_validation_artifact)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data transformation completed")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
